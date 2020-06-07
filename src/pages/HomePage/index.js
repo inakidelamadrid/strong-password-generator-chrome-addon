5bf17bfb956865b3298b2baf15909108058c4939
Ignacio De La Madrid <ignacio.delamadrid@espressive.com>
2020-06-07 10:09:44 -0600

    Add the homepage and import bootstrap's css

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePage = (props) => {
+  return (
+    <div className="page">
+      <h3>Strong password generator</h3>
+    </div>
+  );
+}
+export default HomePage;
