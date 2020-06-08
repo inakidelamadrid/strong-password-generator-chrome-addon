51122df0dc024536f4750c9bb93bd8bfde9e0532
Ignacio De La Madrid <ignacio.delamadrid@espressive.com>
2020-06-07 19:31:12 -0600

    Add section to select alpha/symbols allowed groups and state hook

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -4,10 +4,21 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import styles from './styles.module.css';
 
+const CHARACTER_GROUPS_VALUES = {
+  all: 'all',
+  easyToRead: 'easyToRead',
+  easyToSay: 'easyToSay',
+};
+
 const HomePage = props => {
   const [digitsLength, setDigitsLength] = useState(8);
+  const [selectedCharacterGroup, setSelectedCharacterGroup] = useState(
+    CHARACTER_GROUPS_VALUES.easyToSay,
+  );
 
   const handleDigitsLengthChange = evt => setDigitsLength(evt.target.value);
+  const handleCharacterGroupOptionsChange = changeEvent =>
+    setSelectedCharacterGroup(changeEvent.target.value);
 
   return (
     <div className="page">
@@ -29,6 +40,41 @@ const HomePage = props => {
           <Col xs={1} className={styles.centeredContent}>
             <div>{digitsLength}</div>
           </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="digitsSensitivity">
+          <Col xs={6}>
+            <Form.Check
+              id={'characters_easy_to_say'}
+              label={'Easy to say'}
+              type={'radio'}
+              checked={
+                selectedCharacterGroup === CHARACTER_GROUPS_VALUES.easyToSay
+              }
+              onChange={handleCharacterGroupOptionsChange}
+              value={CHARACTER_GROUPS_VALUES.easyToSay}
+            />
+
+            <Form.Check
+              checked={
+                selectedCharacterGroup === CHARACTER_GROUPS_VALUES.easyToRead
+              }
+              id={'characters_easy_to_read'}
+              label={'Easy to read'}
+              onChange={handleCharacterGroupOptionsChange}
+              type={'radio'}
+              value={CHARACTER_GROUPS_VALUES.easyToRead}
+            />
+
+            <Form.Check
+              checked={selectedCharacterGroup === CHARACTER_GROUPS_VALUES.all}
+              id={'all_characters'}
+              label={'All characters'}
+              onChange={handleCharacterGroupOptionsChange}
+              type={'radio'}
+              value={CHARACTER_GROUPS_VALUES.all}
+            />
+          </Col>
+          <Col xs={6}></Col>
         </Form.Group>
       </Form>
     </div>
