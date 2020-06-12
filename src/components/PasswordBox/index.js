ce5de0f2724129177b8ceadc2c73b55a8aa5c340
Ignacio De La Madrid <ignacio.delamadrid@espressive.com>
2020-06-11 18:58:05 -0600

    Add password box

--- src/components/PasswordBox/index.js
+++ src/components/PasswordBox/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import styles from './styles.module.scss';
 
 const PasswordBox = props => {
   return (
-    <div>
+    <div className={styles.passwordBox}>
+      <div>
+        Some password here
+      </div>
       <div>
         <FontAwesomeIcon icon="copy" size="2x" />
       </div>
       <div>
         <FontAwesomeIcon icon="redo" size="2x" />
       </div>
-      <div></div>
     </div>
   );
 };
