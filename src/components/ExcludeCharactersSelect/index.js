ffa97b39c2a3d99400afc83da0cdd47ade4804a6
Ignacio De La Madrid <ignacio.delamadrid@espressive.com>
2020-06-11 18:12:53 -0600

    Added the select for the excluded chars

--- src/components/ExcludeCharactersSelect/index.js
+++ src/components/ExcludeCharactersSelect/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+// This componnet will customize the select component using the package below
+// https://react-select.com/home
+import React, {useState} from 'react';
 import Form from 'react-bootstrap/Form';
+import Select from 'react-select';
+
+const baseExcludedChars = [
+  {value: '1', label: '1 ("one")'},
+  {value: 'l', label: 'l ("lowercase l")'},
+  {value: '0', label: '0 ("zero")'},
+  {value: 'O', label: 'O ("uppercase o")'},
+];
 
 const ExcludeCharactersSelect = props => {
+  const [options, setOptions] = useState(baseExcludedChars);
+
   return (
     <Form.Group>
       <Form.Label>Exclude the following characters</Form.Label>
-      <Form.Control type="text" />
+      <Select
+        className="basic-multi-select"
+        classNamePrefix="select"
+        defaultValue={baseExcludedChars}
+        isMulti
+        name="excludedChars"
+        options={options}
+      />
     </Form.Group>
   );
 };
