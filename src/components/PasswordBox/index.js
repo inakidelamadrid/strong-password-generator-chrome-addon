1bfa7eb13aae07816991fca65876b3099f2b053e
Ignacio De La Madrid <ignacio.delamadrid@espressive.com>
2020-06-25 16:37:40 -0600

    Style password components

--- src/components/PasswordBox/index.js
+++ src/components/PasswordBox/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import styles from './styles.module.scss';
 
 const PasswordBox = props => {
   return (
-    <div className={styles.passwordBox}>
-      <div>
+    <Row className={styles.passwordBox}>
+      <Col className={styles.password} xs={9}>
         Some password here
-      </div>
-      <div>
-        <FontAwesomeIcon icon="copy" size="2x" />
-      </div>
-      <div>
-        <FontAwesomeIcon icon="redo" size="2x" />
-      </div>
-    </div>
+      </Col>
+      <Col>
+        <div className={styles.centerItems}>
+          <FontAwesomeIcon icon="copy" size="2x" color="#197bff" />
+        </div>
+      </Col>
+      <Col>
+        <div className={styles.centerItems}>
+          <FontAwesomeIcon icon="redo" size="2x" color="#197bff" />
+        </div>
+      </Col>
+    </Row>
   );
 };
 export default PasswordBox;
